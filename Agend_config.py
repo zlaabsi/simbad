d1b9f0b5f9f3c8c6919d1ea2ed0071df118cf40f
FLOWsy <112703532+MarkyPark@users.noreply.github.com>
2023-08-19 19:39:30 +0200

    Update Agend_config.py
    Added initial instructions to become a Marketeer (Blueprint for Research Agents)

--- Agend_config.py
+++ Agend_config.py
@@ -2,31 +2,33 @@
 
 # Agent configuration
 AGENT_CONFIG = {
-    "name": "Aug 17 MS 2606",
-    "description": "AI assistant to solve complex problems",
-    "goal": ["create a photo of a cat"],
+    "name": "Become a Marketing Specialist for Micro Targeting",
+    "description": "Become a Marketing Specialist for Micro Targeting based on ChatGPT4",
+    "goal": ["Become a Marketing Specialist for Micro Targeting based on ChatGPT4"],
     "agent_workflow": "Goal Based Workflow", 
     "constraints": [
-        "~4000 word limit for short term memory.",
-        "Your short term memory is short, so immediately save important information to files.",
         "If you are unsure how you previously did something or want to recall past events, thinking about similar events will help you remember.",
+        "Ensure the tool and args are as per current plan and reasoning",
+        "Exclusively use the tools listed under \"TOOLS\"",
         "No user assistance",
-        "Exclusively use the commands listed in double quotes e.g. \"command name\""
+        "REMEMBER to format your response as JSON, using double quotes ("") around keys and string values, and commas (,) to separate items in arrays and objects. IMPORTANTLY, to use a JSON object as a string in another JSON object, you need to escape the double quotes."
     ],
-    "instruction": [],
-    "tools": [
+    "instruction": [
+        "Use GPT4 to explain the key success factors for a high converting micro-targeting campaign",
+        "Use GPT4 to ask the 5 most important Questions to generate a high converting micro-targeting campaign",
+        "Use GPT4 to explain the key success factors for a Marketeer to understand a given Market and build the leading Service/Product",
+        "Use GPT4 to summarize the key psychological concept to influence customer decisions and Neurolinguistic Programmation",
+        "Use GPT4 to generate a blueprint for any marketeer to run a high conversion Micro-targeting campaign, write a file"
+    ],
+     "tools": [
         {
-            "name": "DuckDuckGo Search Toolkit",
-            "tools": ["DuckDuckGoSearch"]
-        },
-        {   
-            "name": "Image Generation Toolkit",
-            "tools": ["DalleImageGeneration"]
+           "name": "File Toolkit",
+           "tools": ["Read File","Write File"]
         }
     ],
     "iteration_interval": 500,
     "model": "gpt-4",
-    "max_iterations": 25,
+    "max_iterations": 50,
     "schedule": {
         "start_time": "2023-08-14 21:32:00",
         "recurrence_interval": "2 Minutes",
