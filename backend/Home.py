3f57c63c38d6417ba774dec4d3037d8cb5d0d01a
Zakaria Laabsi <52045850+zlaabsi@users.noreply.github.com>
2023-08-21 12:43:27 +0000

    add agents

---
+++ backend/Home.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from langchain.agents import initialize_agent, AgentType
+from langchain.callbacks import StreamlitCallbackHandler
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import DuckDuckGoSearchRun
+
+
+st.sidebar.success("Select an agent above.")
+
+with st.sidebar:
+    openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
+    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
+    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
+    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+
+st.title("🔎 SIMBAD : Synergistic Integration of Marketing with Bot-driven Acquisition and Data-driven decisions")
+
+#st.markdown("#🔎 SIMBAD : Synergistic Integration of Marketing with Bot-driven Acquisition and Data-driven decisions")
+#st.sidebar.header("SIMBAD")
+
+
+
+st.markdown(
+"""
+AARRR - Unleash the Future of Marketing with AI-Enhanced Pirate Metrics!
+
+Dive deep into micro-segmentation effortlessly, delivering pinpointed ads and AI-crafted landing pages. 
+Plus, watch as our advanced autonomous agents research and refine, ushering in the era of self-optimizing funnels. 
+Elevate your marketing game with intelligence with SIMBAD.
+"""
+
+"""
+We're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
+"""
+)
--- backend/pages/SPEC_for_LP_AD.py
+++ backend/pages/SPEC_for_LP_AD.py
@@ -4,21 +4,28 @@ from langchain.callbacks import StreamlitCallbackHandler
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import DuckDuckGoSearchRun
 
+st.sidebar.success("Select an agent above.")
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="langchain_search_api_key_openai", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/2_Chat_with_search.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
-st.title("🔎 SIMBAD - Powerful Marketing Growth Agent")
+#st.title("🔎 Competition Screener")
 
-"""
-We're using `StreamlitCallbackHandler` to display the thoughts and actions of an agent in an interactive Streamlit app.
-"""
+#st.set_page_config(page_title="Competition Screener", page_icon="🔎")
+
+st.markdown("# SPEC for LP/AD")
+st.sidebar.header("SPEC for LP/AD")
+st.write(
+    """Create specification to generate Landing Pages and Ads for micro-segments. Enjoy!"""
+)
+
+
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "assistant", "content": "Hi, I'm a chatbot who can search the web for market competition. Please enter a target product."}
+        {"role": "assistant", "content": "Hi, I'm a Spec Agent agent who can create a specification of required elements for a Performance Ad and a Landing Page, so i can use it to generate them for micro-segments."}
     ]
 
 if target_product := st.chat_input(placeholder="Write a target product, for example : fitness app"):
@@ -29,18 +36,21 @@ if target_product := st.chat_input(placeholder="Write a target product, for exam
         st.info("Please add your OpenAI API key to continue.")
         st.stop()
 
-    # Define your prompt template here
-    competition_prompt = f'''
-    You are a Competition Screener
-    GOALS: Identify the market leaders for {target_product} and their Value Propositions
-    INSTRUCTION: 
-    - Inform me about the latest information about the list of Most popular {target_product}, add the link to the website that you used",
-    - Identify the Value Proposition, Weaknesses and Strengths of each most popular {target_product} in the list. Add a link to your information source. ",
-    CONSTRAINTS :
-    - If you are unsure how you previously did something or want to recall past events, thinking about similar events will help you remember.
+    # Define your prompt template here for Spec Agent
+    spec_agent_prompt = f'''
+    You are a Spec Agent with the role "SPEC for LP/AD".
+    GOALS: Create a specification of required elements for a Performance Ad and a Landing Page, so i can use it to generate them for micro-segments
+
+    constraints :
+    - If you are unsure how you previously did something or want to recall past events, thinking about similar events will help you remember
+    - Ensure the tool and args are as per current plan and reasoning
+
+    instruction : Define a List of required components for a performance Ad
+    
+    Using the information provided, create specifications to generate Landing Pages and Ads for the target product: {target_product}.
     '''
 
-    st.session_state["messages"].append({"role": "assistant", "content": competition_prompt})
+    st.session_state["messages"].append({"role": "assistant", "content": spec_agent_prompt})
 
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, streaming=True)
     search = DuckDuckGoSearchRun(name="Search")
